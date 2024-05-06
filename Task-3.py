2e5f5eb43f45e262d043219a38d71d967983df88
syedfero <168407783+syedfero@users.noreply.github.com>
2024-05-06 22:07:31 +0530

    Add files via upload

---
+++ Task-3.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+# This will act as a simple database
+db = []  
+
+@app.get('/instruments')
+def get_instruments():
+    return db
+
+@app.post('/add')
+def add_instrument(symbol: str):
+    db.append(symbol)
+    return db
+
+@app.patch('/update')
+def update_instrument(index: int, symbol: str):
+    if index >= len(db):
+        raise HTTPException(status_code=400, detail="No symbol found")
+    db[index] = symbol
+    return db
+
+@app.delete('/remove')
+def remove_instrument(index: int):
+    if index >= len(db):
+        raise HTTPException(status_code=400, detail="No symbol found")
+    db.pop(index)
+    return db
